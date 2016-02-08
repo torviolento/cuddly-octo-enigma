3d6af862bf05871f1ce3e1bc1c4a85cc8c6d9569
Valtteri Asikainen <valtteri-asikainen@hotmail.com>
2016-02-08 05:13:58 +0200

    Added testing version for input_handler

--- game/main.py
+++ game/main.py
@@ -1,5 +1,30 @@
+#
+#
+#
+
+import msvcrt
+
+
 class Input:
     def __init__(self):
+        pass
+
+    def input_handler(self):
+        commands = {"A": lambda: print("aa"), "B": lambda: print("bee")}
+        while True:
+            prompt = ord(msvcrt.getwch().upper())
+            print(prompt)
+            if prompt in range(49, 58):
+                self.move(prompt - 48)
+            elif chr(prompt) in commands:
+                commands[chr(prompt)]()
+            elif prompt == 27:
+                break
+            else:
+                pass
+
+    def move(self, key):
+        print("move", key)
         pass
 
 
@@ -9,7 +34,9 @@ class Output:
 
 
 def main():
-    print("Hello world!")
+    print("Start\n")
+    input = Input()
+    input.input_handler()
 
 
 main()
