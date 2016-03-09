5afd299695676001cf56b29867d5548696cebded
torviolento <joelhokkanen@hotmail.com>
2016-03-09 19:48:25 +0200

    added cursestest.py

---
+++ game/Item.py
@@ -0,0 +1,60 @@
+class Item(object):
+    def __init__(self):
+        ALL = []
+        glyph = ""
+        slot = None
+
+    def on_equip(self):
+        pass
+
+    def on_unequip(self):
+        pass
+
+
+class Weapon(Item):
+    slot = "w"
+
+
+class Armor(Item):
+    pass
+
+
+class Potion(Item):
+    def on_use(self):
+        pass
+
+
+class Scroll(Item):
+    def on_use(self):
+        pass
+
+
+class Headgear(Armor):
+    slot = "h"
+
+
+class Torso(Armor):
+    slot = "t"
+
+
+class Gloves(Armor):
+    slot = "g"
+
+
+class Legs(Armor):
+    slot = "l"
+
+
+class Boots(Armor):
+    slot = "b"
+
+# WEAPONS
+
+
+# ARMOR
+
+
+# POTIONS
+
+
+# SCROLLS
